18076e071928d3367377df135cae591e248b183a
Victor José Lopes Navarro <victor.jose.lopes.navarro@gmail.com>
2025-05-11 02:54:15 +0000

    feat: Resolving another algorithm

---
+++ remove-duplicates-from-sorted-array/index.ts
@@ -0,0 +1,13 @@
+function removeDuplicates(nums: number[]): number {
+    if (nums.length <= 1) return nums.length;
+
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[ i ] !== nums[ j ]) {
+            i++;
+            nums[ i ] = nums[ j ];
+        }
+    }
+
+    return i + 1;
+}
